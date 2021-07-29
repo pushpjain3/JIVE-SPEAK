be4a6ab9b540d438670ab3023a3c1e1269daa901
Pushp Jain <pushp.ug20@nsut.ac.in>
2021-07-30 01:24:35 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const outputDiv = document.querySelector("#jive-talk")
+const inputText = document.querySelector("#input-text")
+const serverURL = "https://api.funtranslations.com/translate/jive.json"
+
+
