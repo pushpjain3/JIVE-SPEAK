5a9f550de2f076b261a925a4075a6c904ef1e1ea
Pushp Jain <pushp.ug20@nsut.ac.in>
2021-07-30 01:41:14 +0530

    done with api call

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const outputDiv = document.querySelector("#jive-talk")
-const inputText = document.querySelector("#input-text")
-const serverURL = "https://api.funtranslations.com/translate/jive.json"
+const outputDiv = document.querySelector("#jive-talk");
+const inputText = document.querySelector("#input-text");
+const serverURL = "https://api.funtranslations.com/translate/jive.json";
+const btnTranslate = document.querySelector("#btn-translate");
 
+btnTranslate.addEventListener("click", () => {
+	const url = serverURL + "?" + "text=" + inputText.value;
 
+	fetch(url)
+		.then((response) => response.json())
+		.then((json) => {
+			outputDiv.innerText = json.contents.translated;
+		})
+		.catch((error) => {
+			console.log("Error Occured: ", error);
+			alert("Something Went Wrong! Please try again later.");
+		});
+});
